a8a386583ce6ad8ca3ac8eb7459b49f1357ad410
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-31 17:15:36 +0530

    sync

--- source/nnkeras.py
+++ source/nnkeras.py
@@ -5,8 +5,226 @@ import pandas as pd
 from glob import glob
 import matplotlib.pyplot as plt
 
+from keras.layers import Dense, InputLayer
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+
+# reading the csv file
+data = pd.read_csv('/Users/paramanandbhat/Downloads/Neuralnetworkandhyperparametertuninginkeras-200330-192024/Neural network and hyperparameter tuning in keras/Dataset/emergency_classification.csv')
+
+
+# create random number generator
+seed = 42
+rng = np.random.RandomState(seed)
+
+# looking at first five rows of the data
+data.head()
+
+print(data.head())
+
+# distribution of classes in the dataset
+count  = data['emergency_or_not'].value_counts()
+
+print(count)
+
+# load images and store it in numpy array
+# empty list to store the images
+X = []
+# iterating over each image
+for img_name in data.image_names:
+    # loading the image using its name
+    img = plt.imread('/Users/paramanandbhat/Downloads/Neuralnetworkandhyperparametertuninginkeras-200330-192024/Neural network and hyperparameter tuning in keras/Dataset/images/' + img_name)
+    # saving each image in the list
+    X.append(img)
+    
+# converting the list of images into array
+X=np.array(X)
+
+# storing the target variable in separate variable
+y = data.emergency_or_not.values
+
+# shape of the image
+X.shape
+
+print(X.shape)
+
+# visualizing images
+fig,ax = plt.subplots(nrows=1,ncols=5,figsize=(20,20))
+for i in range(5):
+    ax[i].imshow(X[i*400])
+    ax[i].axis('off')
+
+# plt.show()
+
+## 2. Pre-processing the data
+# converting 3 dimensional image to 1 dimensional image
+X = X.reshape(X.shape[0], 224*224*3)
+X.shape
+
+print(X.shape)
+
+# minimum and maximum pixel values of images
+X.min(), X.max()
+
+print(X.min(), X.max())
+
+# normalizing the pixel values
+X = X / X.max()
+
+# minimum and maximum pixel values of images after normalizing
+X.min(), X.max()
+
+print(X.min(), X.max())
+
+## 3. Creating training and validation set
+# creating a training and validation set
+X_train, X_valid, y_train, y_valid= train_test_split(X,y,test_size=0.3, random_state=seed)
+
+# shape of training and validation set
+(X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape)
+
+print((X_train.shape, y_train.shape), (X_valid.shape, y_valid.shape))
 
 
 
+## 4. Defining the model architecture
+model=Sequential()
+model.add(InputLayer(input_shape=(X_train.shape[1],)))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+# summary of the model
+model.summary()
+
+print(
+model.summary())
+
+## 5. Compiling the model
+# compiling the model
+model.compile(loss='binary_crossentropy', optimizer="sgd", metrics=['accuracy'])
+
+## 6. Training the model
+# training the model
+model_history = model.fit(X_train, y_train, epochs=50, batch_size=128,validation_data=(X_valid,y_valid))
 
 
+## 7. Evaluating model performance
+# getting predictions in the form of class as well as probabilities
+
+predictions = model.predict(X_valid)[:, 0]
+
+
+prediction = predictions.reshape(-1,)
+# converting probabilities to classes
+prediction_int = prediction >= 0.5
+prediction_int = prediction_int.astype(int)
+
+
+# accuracy on validation set
+from sklearn.metrics import accuracy_score
+print('Accuracy on validation set:', accuracy_score(y_valid, prediction_int), '%')
+
+# summarize history for loss
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+# summarize history for accuracy
+plt.plot(model_history.history['accuracy'])
+plt.plot(model_history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+# pull out the original images from the data which correspond to the validation data
+_, valid_vehicles, _, valid_y = train_test_split(data.image_names.values, y, test_size=0.3, random_state=seed)
+
+# get a random index to plot image randomly
+index = rng.choice(range(len(valid_vehicles)))
+
+# get the corresponding image name and probability
+img_name = valid_vehicles[index]
+prob = (predictions * 100).astype(int)[index]
+
+
+# read the image
+img = plt.imread('/Users/paramanandbhat/Downloads/Neuralnetworkandhyperparametertuninginkeras-200330-192024/Neural network and hyperparameter tuning in keras/Dataset/images/' + img_name)
+
+# print probability and actual class
+print('Model is', prob , '% sure that it is emergency')
+print('And actual class is ', valid_y[index])
+
+# plot image
+plt.imshow(img)
+
+# Hyperparameter tuning of neural networks
+
+'''
+<ol>1. change activation function of hidden layer</ol>
+<ol>2. increase hidden neurons</ol>
+<ol>3. increase hidden layers</ol>
+<ol>4. increase number of epochs</ol>
+<ol>5. change optimizer</ol>
+
+'''
+### 1. Changing activation function of hidden layer
+# using relu as activation function in hidden layer
+model=Sequential()
+model.add(InputLayer(input_shape=(X_train.shape[1],)))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+# compiling the model
+model.compile(loss='binary_crossentropy', optimizer="sgd", metrics=['accuracy'])
+
+# summary of the model
+model.summary()
+
+print(
+model.summary())
+
+# training the model for 50 epochs
+model_history = model.fit(X_train, y_train, epochs=50, batch_size=128,validation_data=(X_valid,y_valid))
+
+# getting predictions in the form of class as well as probabilities
+
+predictions = model.predict(X_valid)[:, 0]
+prediction = predictions.reshape(-1,)
+
+# converting probabilities to classes
+prediction_int = prediction >= 0.5
+prediction_int = prediction_int.astype(int)
+
+# accuracy on validation set
+print('Accuracy on validation set:', accuracy_score(y_valid,prediction_int), '%')
+
+# summarize history for loss
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+### 3. Increasing hidden layers
+model=Sequential()
+model.add(InputLayer(input_shape=(X_train.shape[1],)))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+# compiling the model
+model.compile(loss='binary_crossentropy', optimizer="sgd", metrics=['accuracy'])
+
+# model summary
+model.summary()
+
+
+print(model.summary())
